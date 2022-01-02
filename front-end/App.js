2f79a80cc5be02c6d40cdb1c8216be052e880291
Eduarda <eduardaoliveiragomess@gmail.com>
2022-01-02 16:55:59 -0300

    removing comments

--- front-end/App.js
+++ front-end/App.js
@@ -1,20 +1,6 @@
 import * as React from 'react'
 import Routes from './routes'
 
-//class App extends Component {
-//render() {
-//return <Home />
-// }
-//}
-//export default App
-
 export default function App() {
   return <Routes />
 }
-
-//const AppNavigator = createStackNavigator({
-//home: { screen: Home },
-//details: { screen: Details }
-//})
-
-//const App = createAppContainer(AppNavigator)
